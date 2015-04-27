09acc42cc4e773df9ff1ef7641db691a9d868942
Richard Dowinton <richard.dowinton@gmail.com>
2015-04-27 18:20:21 +0100

    Added Python versions classifiers

--- setup.py
+++ setup.py
@@ -15,5 +15,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
     ]
 )
