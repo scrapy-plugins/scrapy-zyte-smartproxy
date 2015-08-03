e09ca04bed399dc2015deeef465a8ab9d163be6f
Pablo Hoffman <pablo@pablohoffman.com>
2015-08-03 12:05:36 -0300

    bump version to match what's on pypi

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='scrapy-crawlera',
-    version='1.0.1',
+    version='1.0.2',
     license='BSD',
     description='Crawlera middleware for Scrapy',
     author='Scrapinghub',
-    author_email='info@scrapinghub.com',
+    author_email='opensource@scrapinghub.com',
     url='https://github.com/scrapinghub/scrapy-crawlera',
     py_modules=['crawlera', 'hubproxy'],
     platforms=['Any'],
