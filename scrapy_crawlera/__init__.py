04f308b554b2ed2601d4206a21096827c1721ec7
Daniel Graña <dangra@gmail.com>
2017-07-04 11:30:02 -0300

    Bump version: 1.2.3 → 1.2.4

--- scrapy_crawlera/__init__.py
+++ scrapy_crawlera/__init__.py
@@ -1,4 +1,4 @@
 from .middleware import CrawleraMiddleware
 
 
-__version__ = '1.2.3'
+__version__ = '1.2.4'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scrapy-crawlera',
-    version='1.2.3',
+    version='1.2.4',
     license='BSD',
     description='Crawlera middleware for Scrapy',
     maintainer='Raul Gallegos',
