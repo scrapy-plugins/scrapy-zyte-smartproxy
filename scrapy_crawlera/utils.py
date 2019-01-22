9e6f40fae6e4ad47e566639534ee4b59588e013c
Henrique Coura <coura.henrique@gmail.com>
2019-01-22 14:51:30 -0200

    Add docs for settings

--- scrapy_crawlera/utils.py
+++ scrapy_crawlera/utils.py
@@ -7,7 +7,7 @@ from itertools import count
 def exp_backoff(step, max):
     """ Exponential backoff time with Full Jitter """
     # this is a numerically stable version of
-    # min(max, step * 2 ** attempt)
+    # random.uniform(0, min(max, step * 2 ** attempt))
     max_attempts = math.log(max / step, 2)
     for attempt in count(0, 1):
         if attempt < max_attempts:
