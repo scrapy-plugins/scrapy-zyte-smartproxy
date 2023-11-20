4d7495ea2e777286e103ead3e0c16ce14dc56e4d
Adrián Chaves <adrian@chaves.io>
2023-11-20 15:51:32 +0100

    Bump version: 2.3.0 → 2.3.1

--- scrapy_zyte_smartproxy/__init__.py
+++ scrapy_zyte_smartproxy/__init__.py
@@ -1,4 +1,4 @@
 from .middleware import ZyteSmartProxyMiddleware
 
 
-__version__ = '2.3.0'
+__version__ = '2.3.1'
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst") as f:
 
 setup(
     name='scrapy-zyte-smartproxy',
-    version='2.3.0',
+    version='2.3.1',
     license='BSD',
     description='Scrapy middleware for Zyte Smart Proxy Manager',
     long_description=readme,
