de1eed5e8622252c94cfce4d95b387774bf346c1
Peng-Yu Chen <pengyu@libstarrify.so>
2024-02-02 13:36:18 +0000

    Fix compatibility with ZAPI proxy mode in response handling
    There's no exact equivalence of `X-Crawlera-Version` in ZAPI. The
    `Zyte-Request-Id` header seems to be the closest match yet it does not
    cover certain types of errors like authentication errors (e.g.
    `curl -v https://example.com/ -x $API_URL`).
    
    Thus `zyte-error-type` is introduced as well to also cover such
    scenarios.

--- scrapy_zyte_smartproxy/middleware.py
+++ scrapy_zyte_smartproxy/middleware.py
@@ -265,7 +265,7 @@ class ZyteSmartProxyMiddleware(object):
         if not self._is_enabled_for_request(request):
             return self._handle_not_enabled_response(request, response)
 
-        if not self._is_zyte_smartproxy_response(response):
+        if not self._is_zyte_smartproxy_or_zapi_response(response):
             return response
 
         key = self._get_slot_key(request)
@@ -365,8 +365,12 @@ class ZyteSmartProxyMiddleware(object):
         parsed = urlparse(url)
         return parsed.netloc
 
-    def _is_zyte_smartproxy_response(self, response):
-        return bool("X-Crawlera-Version" in response.headers)
+    def _is_zyte_smartproxy_or_zapi_response(self, response):
+        return (
+            "X-Crawlera-Version" in response.headers
+            or "Zyte-Request-Id" in response.headers
+            or "zyte-error-type" in response.headers
+        )
 
     def _get_slot_key(self, request):
         return request.meta.get('download_slot')
