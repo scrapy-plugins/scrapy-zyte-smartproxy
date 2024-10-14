06e30dac2997f8c00d384afc7dfd57886d1e88a6
Andrey Rakhmatullin <wrar@wrar.name>
2024-10-14 19:33:05 +0500

    Add Python 3.13 support.

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Framework :: Scrapy',
         'Intended Audience :: Developers',
         'Topic :: Internet :: WWW/HTTP',
