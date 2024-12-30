d84ec729c4706effdb13a5e88ebc20456606cbcc
Adrián Chaves <adrian@chaves.io>
2024-12-30 10:34:45 +0100

    Bump version: 2.3.5 → 2.4.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "rb") as f:
 
 setup(
     name="scrapy-zyte-smartproxy",
-    version="2.3.5",
+    version="2.4.0",
     license="BSD",
     description="Scrapy middleware for Zyte Smart Proxy Manager",
     long_description=readme,
